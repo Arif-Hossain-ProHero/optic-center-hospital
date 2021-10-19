f5b415688a45fceebdd8a29efd02a8ae535b7064
Arif-Hossain-ProHero <arif.programminghero@gmail.com>
2021-10-19 21:30:07 +0600

    service details added

--- src/pages/ServiceDetail/ServiceDetail.js
+++ src/pages/ServiceDetail/ServiceDetail.js
@@ -1,10 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router";
+import SingleService from "../SingleService/SingleService";
 
 const ServiceDetail = () => {
   const { serviceId } = useParams();
+  const [service, setService] = useState([]);
 
-  return <div>This is Service Detail {serviceId}</div>;
+  useEffect(() => {
+    fetch("/fakeDb.json")
+      .then((res) => res.json())
+      .then((data) => {
+        setService(data);
+      });
+  }, []);
+
+  const items = service.filter((s) => s.id == serviceId);
+  console.log(items);
+  return (
+    <div>
+      <div>
+        {items.map((item) => (
+          <SingleService item={item}></SingleService>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default ServiceDetail;
---
+++ src/pages/SingleService/SingleService.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+const SingleService = ({ item }) => {
+  const { title, img, description, treatment } = item;
+  return (
+    <div className="container mx-auto mb-6">
+      <h1 className="text-5xl text-center font-semibold my-12">{title}</h1>
+      <div className="flex md:flex-row flex-col-reverse">
+        <div className="w-3/5 mr-3">
+          <div className=" mb-6">
+            <h2 className="text-3xl mb-4">Description:</h2>
+            <p className="leading-8"></p>
+            {description}
+          </div>
+          <div>
+            <h2 className="text-3xl mb-4">Treatment:</h2>
+            {treatment}
+          </div>
+        </div>
+        <div className="w-2/5">
+          <img className="w-full" src={img} alt="img" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SingleService;
